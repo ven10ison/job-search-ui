4285988afdbcb201fd72ccebeb2c0d968dfcc3ae
Alven Asdal <supportingenterpriseinc.@Supportings-MacBook-Pro.local>
2023-03-22 18:39:50 +0800

    first commit

---
+++ app/index.js
@@ -0,0 +1,60 @@
+import { View, ScrollView, SafeAreaView } from 'react-native';
+import  { useState } from 'react';
+import { Stack, useRouter } from 'expo-router';
+
+import { COLORS, icons, images, SIZES } from '../constants';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components';
+import useFetch from '../hook/useFetch';
+
+
+const Home = () => {
+
+    const { data, isLoading, error } = useFetch('search', {
+        query: "React Developer",
+        num_pages: "1"
+      });
+
+      const [searchTerm, setSearchTerm] = useState("");
+
+    const router = useRouter();
+    return (
+        <SafeAreaView style={{ flex:1, backgroundColor:COLORS.lightWhite}}>
+            <Stack.Screen 
+                options={{ 
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+                    ),
+                    headerTitle: ""
+                 }}
+            />
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View
+                    style={{ 
+                        flex: 1,
+                        padding: SIZES.medium
+                     }} 
+                 >
+                     <Welcome 
+                        searchTerm={searchTerm}
+                        setSearchTerm={setSearchTerm}
+                        handleClick={() => {
+                            if(searchTerm){
+                                router.push(`/search/${searchTerm}`)
+                            }
+                        }}
+                     />
+                     <Popularjobs data={data} isLoading={isLoading} error={error}/>
+                     <Nearbyjobs  data={data} isLoading={isLoading} error={error}/>
+                 </View>
+            </ScrollView>
+        </SafeAreaView>
+    )
+}
+
+export default Home;
